6dc8069b152a908eadc4481158ab5053324a98e7
Shamil092002 <aliverdievsamil@gmail.com>
2023-04-17 12:54:01 +0400

    commit

--- task3.js
+++ task3.js
@@ -2,18 +2,20 @@ let name = prompt('Enter the name of product');
 let unitPrice = prompt('Enter the unit price');
 let quantity = prompt('Enter the quantity');
 let participation = prompt('Is product participate in the promotion');
-let product = unitPrice * 0.1;
-let product2 = unitPrice * 0.15;
-let product3 = unitPrice * 0.25;
+
 function costOfProduct() {
     if (quantity >= 5) {
-        alert('10% discount : ' + (unitPrice - product))
+        alert('10% discount : ' + (unitPrice = unitPrice - unitPrice * 0.1))
     }
     if (participation == 1) {
-        alert('15% discount : ' + (unitPrice - product2))
+        alert('15% discount : ' + (unitPrice = unitPrice - unitPrice * 0.15))
     }
     if (quantity >= 5 && participation == 1) {
-        alert('25% discount : ' + (unitPrice - product3))
+        alert('25% discount : ' + (unitPrice = unitPrice - unitPrice * 0.25))
+    }
+    for(i = 0; i <= quantity; i++){
+        unitPrice = unitPrice * i;
+        console.log(i);
     }
 }
 costOfProduct()
